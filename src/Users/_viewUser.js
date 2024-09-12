86bc7b983676916aa0c5fdedec4cf00676688721
Magesh.B <156915756+Mageshmca2001@users.noreply.github.com>
2024-09-12 23:13:59 +0530

    Update _viewUser.js

--- src/Users/_viewUser.js
+++ src/Users/_viewUser.js
@@ -31,7 +31,7 @@ function ViewUser(props) {
 
     const fetchUserData = async () => {
         try {
-            const response = await axios.get('http://localhost:4000/users/' + props.userId);
+            const response = await axios.get('https://mageshmca2001.github.io/db.json/db.json' + props.userId);
             if (response) {
                 console.log(response.data);
                 setUserInfo(response.data);
